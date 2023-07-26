73bdf830c2fe82ae9c540046f029ac7847ae58ee
hernanfuria <hernan.furiasse@hotmail.com>
2023-07-26 16:59:05 -0300

    Main component OweApp
    Added OweApp.jsx with the OweApp component, which is called from
    main.jsx
    
    Signed-off-by: hernanfuria <hernan.furiasse@hotmail.com>

---
+++ src/components/OweApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const OweApp = () => {
+  return (
+    <div>OweApp</div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { OweApp } from './components/OweApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <OweApp />
   </React.StrictMode>,
 )
