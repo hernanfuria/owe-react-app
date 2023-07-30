58657636207e209a9374efeaa523bfd5e4656d79
hernanfuria <hernan.furiasse@hotmail.com>
2023-07-30 17:04:00 -0300

    Created styles.css
    Signed-off-by: hernanfuria <hernan.furiasse@hotmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { OweApp } from './components/OweApp.jsx'
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
